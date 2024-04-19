8c1403e6d2836ddfed72fb77171a52c3b5d0df0f
Wolfgang Fahl <wf@bitplan.com>
2024-04-19 18:22:05 +0200

    prepares release

--- profiwiki/version.py
+++ profiwiki/version.py
@@ -15,12 +15,12 @@ class Version(object):
     description = """BITPlan's professional Semantic Mediawiki"""
     version = profiwiki.__version__
     date = "2023-04-01"
-    updated = "2023-04-08"
+    updated = "2024-04-19"
     authors = "Wolfgang Fahl"
     doc_url = "https://wiki.bitplan.com/index.php/ProfiWiki"
     chat_url = "https://github.com/BITPlan/ProfiWiki/discussions"
     cm_url = "https://github.com/BITPlan/ProfiWiki"
-    license = f"""Copyright 2015-2023 contributors. All rights reserved.
+    license = f"""Copyright 2015-2024 contributors. All rights reserved.
   Licensed under the Apache License 2.0
   http://www.apache.org/licenses/LICENSE-2.0
   Distributed on an "AS IS" basis without warranties
